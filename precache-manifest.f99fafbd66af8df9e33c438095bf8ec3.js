b465ded3d4bdd2647046a78271994ce7e869ff0e
artem6 <artem6@users.noreply.github.com>
2020-05-02 08:19:40 -0700

    Add files via upload

---
+++ precache-manifest.f99fafbd66af8df9e33c438095bf8ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c97f73b26abd403aab526d1c533976d3",
+    "url": "/acnh_turnips/index.html"
+  },
+  {
+    "revision": "bb1767b4fc97b6cc4309",
+    "url": "/acnh_turnips/static/css/main.6dedb245.chunk.css"
+  },
+  {
+    "revision": "139dd4d44c09b54f33ed",
+    "url": "/acnh_turnips/static/js/2.9feeb4a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/acnh_turnips/static/js/2.9feeb4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1767b4fc97b6cc4309",
+    "url": "/acnh_turnips/static/js/main.5503e51c.chunk.js"
+  },
+  {
+    "revision": "1124f5a3989db6cd6dea",
+    "url": "/acnh_turnips/static/js/runtime-main.e21b3aff.js"
+  }
+]);
